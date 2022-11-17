9bd2e82ae5abdca8232c2039ebc86006ad5c8df7
CaptainDespair <danila.syrokvasov@yandex.ru>
2022-11-17 22:16:19 +0300

    fake-agent is bad also. Proxy?

--- otzovik.py
+++ otzovik.py
@@ -1,6 +1,7 @@
 import requests
 import lxml
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
 url = 'https://irecommend.ru/content/taksa'
 
@@ -12,14 +13,16 @@ raiting = soup.find(class_="average-rating").find('span').text
 votes = soup.find(class_="total-votes").find('span').text
 print(f'Название: {name} \nОценка: {raiting} \nГолосов: {votes}')
 
-headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 YaBrowser/22.11.0.2424 Yowser/2.5 Safari/537.36'}
-
-user_query = ' '.join(['такса', 'irecommend.ru'])        
+#headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 YaBrowser/22.11.0.2424 Yowser/2.5 Safari/537.36'}
+ua = UserAgent()
+headers = "'" + ua.random + "'"
+user_query = ' '.join(['Яндекс такси', 'irecommend.ru'])        
 url_query = '+'.join(user_query.split())                     
 url2 = ''.join(['https://yandex.ru/search/?text=', url_query, '&lr=213'])
-html2 = requests.get(url2, headers=headers, timeout=10)
+html2 = requests.get(url2, headers={'User-agent': headers}, timeout=10)
 soup2 = BeautifulSoup(html2.text, 'lxml')
 site_first = soup2.find("a", {"class":"Link Link_theme_normal OrganicTitle-Link organic__url link"}).get("href")
 print(site_first) 
+
 
 
