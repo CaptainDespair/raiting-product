defee614b988bbd43fcd6569003895ff7e2c033f
CaptainDespair <danila.syrokvasov@yandex.ru>
2022-11-20 02:16:05 +0300

    Nice code. Nothing superfluous

--- irecommend.py
+++ irecommend.py
@@ -1,26 +1,42 @@
 import requests
-import lxml
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
-ua = UserAgent()
-headers = "'" + ua.random + "'"
-user_query = input('Введите название товара: ')     
-url_query = user_query.replace(' ', '%20')                     
-url2 = ''.join(['https://irecommend.ru/srch?query=', url_query])
-html2 = requests.get(url2, headers={'User-agent': headers}, timeout=10)
-soup2 = BeautifulSoup(html2.text, 'lxml')
 
-title = soup2.find("div", {"class":'title'}).text
-raiting = soup2.find(class_="average-rating").find('span').text
-votes = soup2.find(class_ = "read-all-reviews-link").find(class_ ="counter").text
-
-print(f'Название: {title} \nОценка: {raiting} \nГолосов: {votes}')
+def search_pattern(url, user_query):     
+    url_query = user_query.replace(' ', '%20')                     
+    new_url = ''.join([url, url_query])
+    return new_url
 
 
+def get_html(new_url):
+    ua = UserAgent()
+    headers = "'" + ua.random + "'"
+    html = requests.get(new_url, headers={'User-agent': headers}, timeout=10)
+    return html
 
 
+def html_data(html):
+    try:
+        soup = BeautifulSoup(html.text, 'lxml')
+        title = soup.find("div", {"class":'title'}).text
+        raiting = soup.find(class_="average-rating").find('span').text
+        votes = soup.find(class_ = "read-all-reviews-link").find(class_ ="counter").text
+        return f'Название: {title} \nОценка: {raiting} \nГолосов: {votes}'
+    except:
+        return ('Error!')
+
+def main():
+    url = 'https://irecommend.ru/srch?query='
+    user_query = input('Введите название товара: ')
+    print(
+        html_data(
+            get_html(
+                (search_pattern(url, user_query))
+            )
+        )
+    )
 
 
-
-
+if __name__ == '__main__':
+    main()
